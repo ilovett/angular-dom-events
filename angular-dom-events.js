7bac017e012485c892565649230dfe4bb3fd1b4b
brunopiovan <brunopiovan@gmail.com>
2014-11-01 16:46:12 -0200

    Make it minify-able

--- angular-dom-events.js
+++ angular-dom-events.js
@@ -1,44 +1,43 @@
-(function(window, angular, undefined) {
+(function (window, angular, undefined) {
+    'use strict';
 
-  'use strict';
-
-  angular.module('ngDomEvents', [])
+    angular.module('ngDomEvents', [])
     /**
-     * @ngdoc directive
-     * @name ngDomEvents.directive:domOnCreate
-     * @description
-     * # domOnCreate
-     */
-    .directive('domOnCreate', function($parse, $timeout) {
-      return {
-        link: function postLink(scope, element, attrs) {
-          // wrap in a timeout to allow directives to link scope callbacks
-          $timeout(function() {
-            if (attrs.domOnCreate) {
-              $parse(attrs.domOnCreate)(scope);
+    * @ngdoc directive
+    * @name ngDomEvents.directive:domOnCreate
+    * @description
+    * # domOnCreate
+    */
+    .directive('domOnCreate', ['$parse', '$timeout', function ($parse, $timeout) {
+        return {
+            link: function postLink(scope, element, attrs) {
+                // wrap in a timeout to allow directives to link scope callbacks
+                $timeout(function () {
+                    if (attrs.domOnCreate) {
+                        $parse(attrs.domOnCreate)(scope);
+                    }
+                });
             }
-          });
-        }
-      };
-    })
-    /**
-     * @ngdoc directive
-     * @name ngDomEvents.directive:domOnDestroy
-     * @description
-     * # domOnDestroy
-     */
-    .directive('domOnDestroy', function($parse) {
-      return {
-        link: function postLink(scope, element, attrs) {
-          var destroyHandler;
-          if (attrs.domOnDestroy) {
-            destroyHandler = $parse(attrs.domOnDestroy);
-            element.on('$destroy', function() {
-              destroyHandler(scope);
-            });
-          }
-        }
-      };
-    })
+        };
+    }])
 
+    /**
+    * @ngdoc directive
+    * @name ngDomEvents.directive:domOnDestroy
+    * @description
+    * # domOnDestroy
+    */
+    .directive('domOnDestroy', ['$parse', function ($parse) {
+        return {
+            link: function postLink(scope, element, attrs) {
+                var destroyHandler;
+                if (attrs.domOnDestroy) {
+                    destroyHandler = $parse(attrs.domOnDestroy);
+                    element.on('$destroy', function () {
+                        destroyHandler(scope);
+                    });
+                }
+            }
+        };
+    }])
 })(window, window.angular);
